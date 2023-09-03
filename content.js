b8d59649413161f5f6fcecfccd5361f38d32b5ee
Mani-Ravirala <raviralamani5@gmail.com>
2023-09-03 20:06:17 +0530

    Final commit to publish

--- content.js
+++ content.js
@@ -1,27 +1,20 @@
 var usernameField = document.querySelector("input[name='username']");
 var passwordField = document.querySelector("input[name='password']");
 const submitButton = document.querySelector("#loginbtn");
+const loginFastBtn = document.querySelector("#button_1");
 
 function set() {
-    const a = localStorage.getItem('username');
-    const b = localStorage.getItem('password');
-  if(a!=usernameField.value && b!=passwordField.value) {
-    localStorage.setItem("username", usernameField.value.split("@")[0]);
-    localStorage.setItem("password", passwordField.value);
-    filldet()
-  }
+  localStorage.setItem("username", usernameField.value.split("@")[0]);
+  localStorage.setItem("password", passwordField.value);
 }
 
-function filldet(){
-    const username = localStorage.getItem('username')
-    const password = localStorage.getItem('password')
-    fillLoginForm(username,password)
+function filldet() {
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
+  fillLoginForm(username, password);
 }
 
-
-submitButton.addEventListener("click", set);
-
-function fillLoginForm(username , password) {
+function fillLoginForm(username, password) {
   var agreeCheckbox = document.querySelector("#agreepolicy");
 
   if (usernameField && passwordField && submitButton) {
@@ -33,6 +26,23 @@ function fillLoginForm(username , password) {
   }
 }
 
+// window.addEventListener("load", filldet);
 
+function runExtension() {
+  if (submitButton) {
+    const button = document.createElement("button");
+    button.innerText = "Login Fast";
+    button.id = "button_1";
+    button.className = "btn btn-primary btn-sm";
+    button.style.marginLeft = "10px";
+    const element = document.querySelector(".text-center.mt-2");
+    element.appendChild(button);
+    button.addEventListener("click", function () {
+      filldet();
+    });
+  }
+}
 
-window.addEventListener("load", filldet);
+runExtension();
+
+submitButton.addEventListener("click", set);
