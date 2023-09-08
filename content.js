c3a861bbf4c67cdb908d81f6b14774062031fb62
Mani-Ravirala <raviralamani5@gmail.com>
2023-09-08 23:51:19 +0530

    Added Telegram channel advertisement

--- content.js
+++ content.js
@@ -43,6 +43,47 @@ function runExtension() {
   }
 }
 
+function advTelegram() {
+  // Create the banner container
+  const advdiv1 = document.createElement("div");
+  advdiv1.className = "banner-container";
+
+  // Create the banner text
+  const bannerText = document.createElement("div");
+  bannerText.className = "banner-text";
+
+  // Create the heading and paragraph elements
+  const heading = document.createElement("h2");
+  // heading.textContent = "Join our Telegram channel for amazing deals!";
+  heading.innerHTML = "Join Amazeondeals on Telegram for amazing deals!"; // Modify this line to add the name "Amazeondeals"
+
+  const paragraph = document.createElement("p");
+  paragraph.textContent =
+    "Get exclusive discounts and offers on a wide range of products.";
+
+  // Append the heading and paragraph elements to the banner text
+  bannerText.appendChild(heading);
+  bannerText.appendChild(paragraph);
+
+  // Create the banner button
+  const bannerButton = document.createElement("a");
+  bannerButton.href = "https://telegram.me/amazeondeals";
+  bannerButton.target = "_blank";
+  bannerButton.className = "banner-button";
+  bannerButton.textContent = "Join Now";
+
+  // Append the banner text and button to the banner container
+  advdiv1.appendChild(bannerText);
+  advdiv1.appendChild(bannerButton);
+
+  // Find the element to append the banner container to
+  const element = document.querySelector(".text-center.pt-4.pb-3");
+
+  // Append the banner container to the element
+  element.appendChild(advdiv1);
+}
+
+advTelegram();
 runExtension();
 
 submitButton.addEventListener("click", set);
